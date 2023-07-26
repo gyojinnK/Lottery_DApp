7ad1e70c1c6bce6f78b05295f026fd616d14f3f5
gyojinnK <ksmug1998@gmail.com>
2023-07-27 01:51:17 +0900

    _bets에 대한 push, pop 함수 생성

--- lottery_smart_contract/contracts/Lottery.sol
+++ lottery_smart_contract/contracts/Lottery.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Lottery{
     struct BetInfo{
         uint256 answerBlockNumber;
-        address payable better;
-        bytes challenges;
+        address payable bettor;
+        bytes1 challenges;
     }
 
     uint256 private _tail;
@@ -13,7 +13,7 @@ contract Lottery{
     mapping(uint256 => BetInfo) private _bets;
 
     address public owner;
-    
+
     uint256 constant internal BLOCK_LIMIT = 256;
     uint256 constant internal BET_BLOCK_INTERVAL = 3;
     uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
@@ -29,5 +29,36 @@ contract Lottery{
 
     function getPot() public view returns (uint256 value) {
         return _pot;
+    }
+
+    // Bet
+    // - save the bet to the queue
+
+    // Distribute
+    // - check the answer
+
+    function getBetInfo(uint256 index) public view returns (uint256 answerBlockNumber, address bettor, bytes1 challenges){
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        bettor = b.bettor;
+        challenges = b.challenges;
+    }
+
+    function pushBet(bytes1 challenges) public returns (bool){
+        BetInfo memory b;
+        b.bettor = payable(msg.sender);
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
+        b.challenges = challenges;
+
+        _bets[_tail] = b;
+        _tail++;
+
+        return true;
+    }
+
+    function popBet(uint256 index) public returns (bool){
+        // delete하면 데이터를 저장하지 않겠다는 의미로 일정량의 가스비를 돌려받음
+        delete _bets[index];
+        return true;
     }
 }
