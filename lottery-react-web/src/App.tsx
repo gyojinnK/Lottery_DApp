79ea0715d81f493c0851f30aa3e3648b733ae15f
gyojinnK <ksmug1998@gmail.com>
2023-07-30 02:22:03 +0900

    web3 객체로 코드 수정

--- lottery-react-web/src/App.tsx
+++ lottery-react-web/src/App.tsx
@@ -1,32 +1,50 @@
-import React from "react";
-import "./App.css";
-import { useEtherBalance, useEthers } from "@usedapp/core";
-import { formatEther } from "@ethersproject/units";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
 
-const App = () => {
-    const { activateBrowserWallet, deactivate, account } = useEthers();
-    const etherBalance = useEtherBalance(account);
+const App: React.FC = () => {
+    const [account, setAccount] = useState("");
+    const [balance, setBalance] = useState("");
+
+    useEffect(() => {
+        if (window.ethereum) {
+            const web3 = new Web3(window.ethereum);
+            connectMetaMask(web3);
+        } else {
+            alert(
+                "MetaMask is not installed. Please install it and try again."
+            );
+        }
+    }, []);
+
+    const connectMetaMask = async (web3: Web3) => {
+        try {
+            await window.ethereum.enable();
+            const accounts = await web3.eth.getAccounts();
+            const balances: string = (
+                await web3.eth.getBalance(accounts[0])
+            ).toString();
+            setAccount(accounts[0]);
+            setBalance(balances);
+        } catch (error) {
+            alert("User denied account access");
+        }
+    };
 
     return (
         <div className="App">
             <header className="App-header">
-                {account && `Account: ${account}`}
-                <br />
-                {etherBalance &&
-                    `Balance: ${Number(formatEther(etherBalance)).toFixed(
-                        4
-                    )} ETH`}
-                <p className="App-link">
-                    {account ? (
-                        <code onClick={deactivate}>LogOut</code>
-                    ) : (
-                        <code onClick={() => activateBrowserWallet()}>
-                            Connect Wallet
-                        </code>
-                    )}
-                </p>
+                <h1>MetaMask 연동 예제</h1>
+                {account ? (
+                    <div>
+                        <p>Wallet address: {account}</p>
+                        <p>Wallet balance: {balance} ETH</p>
+                    </div>
+                ) : (
+                    <p>지갑이 연결되지 않았습니다.</p>
+                )}
             </header>
         </div>
     );
 };
+
 export default App;
